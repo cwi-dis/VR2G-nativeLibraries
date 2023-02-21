c00ea18be60c561963f45a62b67d436fe6a894bd
MALAKOV\dis <cwidisgroup@gmail.com>
2023-02-21 17:11:14 +0100

    Added a ScriptableObject to define the toplevel folder (in the editor).

---
+++ VRTNativeLibraries/Plugins/NativeLibraryDirectory.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LibraryDirectory", menuName = "ScriptableObjects/NativeLibraryDirectory", order = 1)]
+public class NativeLibraryDirectory : ScriptableObject
+{
+   
+}
--- VRTNativeLibraries/Scripts/VRTNativeLibraries.cs
+++ VRTNativeLibraries/Scripts/VRTNativeLibraries.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 namespace VRT.NativeLibraries
 {
     public class VRTNativeLibraries : MonoBehaviour
     {
+        public NativeLibraryDirectory nativeLibraries;
         void Awake()
         {
             Init();
         }
 
-        public static void Init()
+        public void Init()
         {
             Debug.Log("VRT.NativeLibraries.Support: Init() called");
+            var path = UnityEditor.AssetDatabase.GetAssetPath(MonoScript.FromScriptableObject(nativeLibraries));
+            Debug.Log($"VRT.NativeLibraries.Support: path = {path}");
         }
     }
 }
