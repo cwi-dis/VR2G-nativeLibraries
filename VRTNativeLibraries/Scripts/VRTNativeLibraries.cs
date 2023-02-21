f65ddc73ef249fa72145bacd3f2006fc5de0c0d6
Jack Jansen <Jack.Jansen@cwi.nl>
2023-02-21 14:54:02 +0100

    Getting started with supporting included native libraries and setting search path. Mainly boilerplate.

---
+++ VRTNativeLibraries/Scripts/VRTNativeLibraries.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VRT.NativeLibraries
+{
+    public class VRTNativeLibraries : MonoBehaviour
+    {
+        void Awake()
+        {
+            Init();
+        }
+
+        public static void Init()
+        {
+            Debug.Log("VRT.NativeLibraries.Support: Init() called");
+        }
+    }
+}
