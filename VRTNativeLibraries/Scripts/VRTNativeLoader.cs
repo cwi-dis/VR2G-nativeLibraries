6f5565c95ca71f5ee654b34444c1de87632384e8
MALAKOV\dis <cwidisgroup@gmail.com>
2023-02-22 12:25:25 +0100

    Restructuring.

---
+++ VRTNativeLibraries/Scripts/VRTNativeLoader.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace VRT.NativeLibraries
+{
+    public class VRTNativeLoader : MonoBehaviour
+    {
+        public NativeLibraryDirectory nativeLibraries;
+        private string nativeLibrariesPath;
+        private string platformLibrariesPath;
+        void Awake()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            Debug.Log("VRTNativeLoader: Init() called");
+#if UNITY_EDITOR
+            string path = UnityEditor.AssetDatabase.GetAssetPath(nativeLibraries);
+            nativeLibrariesPath = Path.GetDirectoryName(path);
+#else
+            nativeLibrariesPath = "DLLs";
+#endif
+            Debug.Log($"VRTNativeLoader: path = {nativeLibrariesPath}");
+
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            platformLibrariesPath = Path.Combine(nativeLibrariesPath, "win-x64");
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+            platformLibrariesPath = Path.Combine(nativeLibrariesPath, "mac-x64");
+
+#elif UNITY_EDITOR_LINUX || UNITY_STANDALONE_LINUX
+            platformLibrariesPath = Path.Combine(nativeLibrariesPath, "linux-x64");
+#elif UNITY_ANDROID
+            platformLibrariesPath = Path.Combine(nativeLibrariesPath, "android-arm");
+#else
+            Debug.LogFatal("VRTNativeLoader: Unknown runtime platform");
+#endif
+            Debug.Log($"VRTNativeLoader: path = {platformLibrariesPath}");
+
+        }
+    }
+}
