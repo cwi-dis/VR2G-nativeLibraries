f78a8e22685d80eb74ed73a2f35c4a65ac48c7d9
BEELZEBUB\DIS <cwidisgroup@gmail.com>
2025-01-10 16:01:34 +0100

    Revamped the code to add the native plugin directory to the search path. Add to the relevant environment for the platform (PATH, LD_LIBRARY_PATH, DYLD_LIBRARY_PATH) and on Windows also pass to AddDllDirectory().

--- nl.cwi.dis.vr2gather.nativelibraries/Runtime/Scripts/VRTNativeLoader.cs
+++ nl.cwi.dis.vr2gather.nativelibraries/Runtime/Scripts/VRTNativeLoader.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEditor;
+using System.Runtime.InteropServices;
+
 
 namespace VRT.NativeLibraries
 {
     public class VRTNativeLoader : MonoBehaviour
     {
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern bool AddDllDirectory(string lpPathName);
+
         public NativeLibraryDirectory nativeLibraries;
         private string platformLibrariesPath;
         void Awake()
@@ -39,8 +45,44 @@ namespace VRT.NativeLibraries
 #else
             Debug.LogFatal("VRTNativeLoader: Unknown runtime platform");
 #endif
-            Debug.Log($"VRTNativeLoader: path = {platformLibrariesPath}");
+            Debug.Log($"VRTNativeLoader: platform path = {platformLibrariesPath}");
+            platformLibrariesPath = Path.GetFullPath(platformLibrariesPath);
+            Debug.Log($"VRTNativeLoader: abs platform path = {platformLibrariesPath}");
+            if (!Directory.Exists(platformLibrariesPath))
+            {
+                Debug.LogError($"VRTNativeLoader: Directory {platformLibrariesPath} does not exist");
+                return;
+            }
+            addPathToDynamicLoaderPath(platformLibrariesPath);
+        }
+        void addPathToDynamicLoaderPath(string path)
+        {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            const string dynPathName = "PATH";
+#elif UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+            const string dynPathName = "DYLD_LIBRARY_PATH";
+#else
+            const string dynPathName = "LD_LIBRARY_PATH";
+#endif
 
+            string dynamicLoaderPath = System.Environment.GetEnvironmentVariable(dynPathName);
+            if (dynamicLoaderPath == null)
+            {
+                dynamicLoaderPath = path;
+            }
+            else
+            {
+                if (dynamicLoaderPath.Contains(path))
+                {
+                    return;
+                }
+                dynamicLoaderPath = path + Path.PathSeparator + dynamicLoaderPath;
+            }
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            AddDllDirectory(path);
+#endif
+            System.Environment.SetEnvironmentVariable(dynPathName, dynamicLoaderPath);
+            Debug.Log($"VRTNativeLoader: {dynPathName} = {dynamicLoaderPath}");
         }
     }
 }
